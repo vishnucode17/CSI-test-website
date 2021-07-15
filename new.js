99c9e341fc6e2c2f65fbba486065bf53b60aa67e
Phoenix-Robotics-Club <vishuvishnu1717@gmail.com>
2021-07-16 02:55:27 +0530

    new UI design

---
+++ new.js
@@ -0,0 +1,8 @@
+var color_change = ()=>{
+    var txt = document.getElementById('welcome');
+    txt.style.color="blue";
+}
+var normal_color = ()=>{
+    var txt = document.getElementById('welcome');
+    txt.style.color="white";
+}
