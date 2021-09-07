33292152401d912aced42b4e5b6cc491686ac0fc
Phoenix-Robotics-Club <vishuvishnu1717@gmail.com>
2021-09-07 17:20:56 +0530

    Added Mobile site

--- new.js
+++ new.js
@@ -1,8 +1,16 @@
-var color_change = ()=>{
-    var txt = document.getElementById('welcome');
-    txt.style.color="blue";
-}
-var normal_color = ()=>{
-    var txt = document.getElementById('welcome');
-    txt.style.color="white";
-}
+var menu = document.getElementsByClassName('hamburger')[0];
+var hamburger = document.getElementsByClassName('hamburger')[0];
+var nav=document.getElementsByTagName('nav')[0];
+      function show_menu() {
+        if (hamburger.style.display === 'flex'){
+          document.body.style.overflow='visible';
+          hamburger.style.display = 'none';
+          nav.style.height='0';
+          nav.style.padding='0';
+        }
+        else{
+          hamburger.style.display = 'flex';
+          document.body.style.overflow='hidden';
+          hamburger.style.textAlign='center';
+        }
+      }
